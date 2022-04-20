e89566a680d00f434d772e510c40799f7536bd76
sharathkumarsripathi <sharathkumarsripathi@gmail.com>
2022-04-20 16:11:59 +0530

    Update CicdServiceApplicationTests.java

--- src/test/java/com/in10s/CicdServiceApplicationTests.java
+++ src/test/java/com/in10s/CicdServiceApplicationTests.java
@@ -3,6 +3,10 @@ package com.in10s;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*  
+sample class
+*/
+
 @SpringBootTest
 class CicdServiceApplicationTests {
 
